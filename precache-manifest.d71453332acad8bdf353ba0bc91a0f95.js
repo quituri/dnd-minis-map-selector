f6dcacd6dd472d1c113df3042851241903767afb
Quituri <macbookair@MacBooks-MacBook-Air.local>
2022-04-12 14:51:15 -0600

    first commit

---
+++ precache-manifest.d71453332acad8bdf353ba0bc91a0f95.js
@@ -0,0 +1,350 @@
+self.__precacheManifest = [
+  {
+    "revision": "d05b3d95c715e9024e12",
+    "url": "/static/css/main.93600925.chunk.css"
+  },
+  {
+    "revision": "d05b3d95c715e9024e12",
+    "url": "/static/js/main.3a774ebc.chunk.js"
+  },
+  {
+    "revision": "d6caf280973a68c0a4c8",
+    "url": "/static/js/runtime~main.d9770b2d.js"
+  },
+  {
+    "revision": "5c359dbfda3cf5ddf141",
+    "url": "/static/css/2.5d7ad6c6.chunk.css"
+  },
+  {
+    "revision": "5c359dbfda3cf5ddf141",
+    "url": "/static/js/2.e49de767.chunk.js"
+  },
+  {
+    "revision": "39112719ae8304b397a6",
+    "url": "/static/js/3.232ba9ad.chunk.js"
+  },
+  {
+    "revision": "413911e7ba0818954654a91be4598c33",
+    "url": "/static/media/blue-dragon-lair.413911e7.jpg"
+  },
+  {
+    "revision": "e4d688a6bbc2dbd6d828231044b43e73",
+    "url": "/static/media/broken-demongate.e4d688a6.jpg"
+  },
+  {
+    "revision": "0244fbc541f9116a9ead4214b82726bb",
+    "url": "/static/media/caves-of-chaos.0244fbc5.jpg"
+  },
+  {
+    "revision": "86b48798b4fdaee4a83c9f4dc34235d8",
+    "url": "/static/media/dragon-graveyard.86b48798.jpg"
+  },
+  {
+    "revision": "d54472fa7da267025bbaa6f1d5343515",
+    "url": "/static/media/dragon-shrine.d54472fa.jpg"
+  },
+  {
+    "revision": "f25de6ec6762a678ab40d6bc84732e05",
+    "url": "/static/media/dragondown-grotto.f25de6ec.jpg"
+  },
+  {
+    "revision": "75f9439b48d12d05c16c8864ac24a8ae",
+    "url": "/static/media/dragons-hoard.75f9439b.jpg"
+  },
+  {
+    "revision": "8076f68356ac6c17fa48109d0f6ec6a2",
+    "url": "/static/media/drow-enclave.8076f683.jpg"
+  },
+  {
+    "revision": "af8f29cfc014e6ab4cf2b564a8c6f614",
+    "url": "/static/media/drow-outpost.af8f29cf.jpg"
+  },
+  {
+    "revision": "f9859ea028d43685a49914c0dcbf9796",
+    "url": "/static/media/dungeon-of-blood.f9859ea0.jpg"
+  },
+  {
+    "revision": "15e6d3e7bb4103e3939a8b51a908018f",
+    "url": "/static/media/dwarven-outpost.15e6d3e7.jpg"
+  },
+  {
+    "revision": "603814bd3de303e07f7c541ac2a8d9fc",
+    "url": "/static/media/evermelt.603814bd.jpg"
+  },
+  {
+    "revision": "8f5891185eec28c1a4ac13cda1885355",
+    "url": "/static/media/fallen-fortress.8f589118.jpg"
+  },
+  {
+    "revision": "f26419103311339d8782e59e5bb1d6c1",
+    "url": "/static/media/fane-of-lolth.f2641910.jpg"
+  },
+  {
+    "revision": "235d91961958bf5d10b38ebd68f369d6",
+    "url": "/static/media/field-of-ruin.235d9196.jpg"
+  },
+  {
+    "revision": "778ca234ca2f2ee5e6a5e04677632437",
+    "url": "/static/media/flooded-ruins.778ca234.jpg"
+  },
+  {
+    "revision": "f391985a83839082c45235f0d7621b9b",
+    "url": "/static/media/forest-cliff-lair.f391985a.jpg"
+  },
+  {
+    "revision": "f6a03911afb15c46d55eee3ffeed1c78",
+    "url": "/static/media/frostfell-rift.f6a03911.jpg"
+  },
+  {
+    "revision": "e6c544645fb7724a0bfe9ee8e811a127",
+    "url": "/static/media/hailstorm-tower.e6c54464.jpg"
+  },
+  {
+    "revision": "1e35d11ae07924193f7aca5ddbcadf52",
+    "url": "/static/media/hellspike.1e35d11a.jpg"
+  },
+  {
+    "revision": "1cc6f312511c6f9c1b34f6fde22b151f",
+    "url": "/static/media/icingdeaths-lair.1cc6f312.jpg"
+  },
+  {
+    "revision": "9f839a88dae9481234e4974ee234b427",
+    "url": "/static/media/jungle-temple.9f839a88.jpg"
+  },
+  {
+    "revision": "6c02c423780a6c211553d2f4908a0fb4",
+    "url": "/static/media/keep-of-the-fallen-king.6c02c423.jpg"
+  },
+  {
+    "revision": "7782c915c00b5623b8b3e63a77ae759c",
+    "url": "/static/media/kings-road.7782c915.jpg"
+  },
+  {
+    "revision": "6ed6da560e4d0b633393a4c8ca77f7cc",
+    "url": "/static/media/lost-temple.6ed6da56.png"
+  },
+  {
+    "revision": "a238bd9d2bd80629899106e16019ce5f",
+    "url": "/static/media/magma-keep.a238bd9d.jpg"
+  },
+  {
+    "revision": "3cce6df9c998d42bf4434d4ca5860b36",
+    "url": "/static/media/market-square.3cce6df9.jpg"
+  },
+  {
+    "revision": "6279cd2fdb5957bc18bbd6b220d80491",
+    "url": "/static/media/mithral-mines.6279cd2f.jpg"
+  },
+  {
+    "revision": "5a86551e3ab50cd3254ddd78900b5666",
+    "url": "/static/media/mushroom-cavern.5a86551e.jpg"
+  },
+  {
+    "revision": "bca3f9550b3400a6241b37c5843b02a0",
+    "url": "/static/media/pillars-of-heaven.bca3f955.jpg"
+  },
+  {
+    "revision": "4bc570bb46a8c0fc29295b7b5308b317",
+    "url": "/static/media/pirate-cove.4bc570bb.jpg"
+  },
+  {
+    "revision": "a7426ab1350c92213cd89370a06773b9",
+    "url": "/static/media/ratfang-sewers.a7426ab1.jpg"
+  },
+  {
+    "revision": "fd5e9db9a6a97adc69a9957848b6d40e",
+    "url": "/static/media/red-dragon-lair-2.fd5e9db9.jpg"
+  },
+  {
+    "revision": "9fe5878549f607329af39cd0192b4441",
+    "url": "/static/media/red-dragon-lair.9fe58785.jpg"
+  },
+  {
+    "revision": "1a87530f7c122755d30a88d8dbeb9066",
+    "url": "/static/media/teleport-temple.1a87530f.jpg"
+  },
+  {
+    "revision": "269a6ed9586fc8927925054527cf336b",
+    "url": "/static/media/temple-of-the-unseeing.269a6ed9.jpg"
+  },
+  {
+    "revision": "d372ad0e2105570af52734f9f2025605",
+    "url": "/static/media/thieves-quarter.d372ad0e.jpg"
+  },
+  {
+    "revision": "7054c5cd5e23b39ece6fdc38334df03f",
+    "url": "/static/media/tomb-of-queen-peregrine.7054c5cd.jpg"
+  },
+  {
+    "revision": "e017ae4239787ccc6ed4a315221e58e6",
+    "url": "/static/media/underground-grotto.e017ae42.jpg"
+  },
+  {
+    "revision": "d992ff7c6f264583402f264396b62287",
+    "url": "/static/media/windswept-precipice.d992ff7c.jpg"
+  },
+  {
+    "revision": "07eb1e7ba0c7fd59a5b678bb3afb4806",
+    "url": "/static/media/1.07eb1e7b.jpg"
+  },
+  {
+    "revision": "c4b9453bee9580473518ec12e326e332",
+    "url": "/static/media/10.c4b9453b.jpg"
+  },
+  {
+    "revision": "a28da9c536dba44068731de5cc7c1f7b",
+    "url": "/static/media/11.a28da9c5.jpg"
+  },
+  {
+    "revision": "36b6a0d8c1d4a1dc2487b3c928b30890",
+    "url": "/static/media/12.36b6a0d8.jpg"
+  },
+  {
+    "revision": "f4209a2d8cd871ef5b05d31855ce9b68",
+    "url": "/static/media/13.f4209a2d.jpg"
+  },
+  {
+    "revision": "0f78f21c8ece1350b0c9562e32503b29",
+    "url": "/static/media/14.0f78f21c.jpg"
+  },
+  {
+    "revision": "0e84ebad6b677e0ddc46f26964ee2672",
+    "url": "/static/media/15.0e84ebad.jpg"
+  },
+  {
+    "revision": "b591c1726ff6ba402ad5382ae15d98e7",
+    "url": "/static/media/16.b591c172.jpg"
+  },
+  {
+    "revision": "767950c7fa1b03bc9c77318d35ac25ca",
+    "url": "/static/media/17.767950c7.jpg"
+  },
+  {
+    "revision": "e4402ba39981ea55a2eb6cb7e5e35a5f",
+    "url": "/static/media/18.e4402ba3.jpg"
+  },
+  {
+    "revision": "20f18ce0e42bcd92a00de3c14dab0f8e",
+    "url": "/static/media/19.20f18ce0.jpg"
+  },
+  {
+    "revision": "dd0a6a6f12a7b295d46cedc8b0c4cfc1",
+    "url": "/static/media/2.dd0a6a6f.jpg"
+  },
+  {
+    "revision": "977dabe5485c5144572f1432c9f8fecd",
+    "url": "/static/media/20.977dabe5.jpg"
+  },
+  {
+    "revision": "90b42321b3ba18d6acf43472d6b0902a",
+    "url": "/static/media/21.90b42321.jpg"
+  },
+  {
+    "revision": "43c7884bce52a565e96e0b2727fb321f",
+    "url": "/static/media/22.43c7884b.jpg"
+  },
+  {
+    "revision": "f13afc7b585bf98c3b261160e3aec7b9",
+    "url": "/static/media/23.f13afc7b.jpg"
+  },
+  {
+    "revision": "84b61fa9d7f847049cf31d4d7814fa4c",
+    "url": "/static/media/24.84b61fa9.jpg"
+  },
+  {
+    "revision": "12a5d795e98e6e5a8267067ff84de2f8",
+    "url": "/static/media/25.12a5d795.jpg"
+  },
+  {
+    "revision": "73bb95a80eaab839fe218b703ad00a3a",
+    "url": "/static/media/26.73bb95a8.jpg"
+  },
+  {
+    "revision": "5b3f8e161d7e42cce0405b413dbb2288",
+    "url": "/static/media/27.5b3f8e16.jpg"
+  },
+  {
+    "revision": "b9877a9a07bfa680f41e1c2d5f0f590a",
+    "url": "/static/media/28.b9877a9a.jpg"
+  },
+  {
+    "revision": "0fa9c42376cd6139d6b37d561c105426",
+    "url": "/static/media/29.0fa9c423.jpg"
+  },
+  {
+    "revision": "a7c4964be8ec3e0aba41726c31b303e4",
+    "url": "/static/media/3.a7c4964b.jpg"
+  },
+  {
+    "revision": "daa40bed476c745134e49a21f96a838c",
+    "url": "/static/media/30.daa40bed.jpg"
+  },
+  {
+    "revision": "1e6a7afe1d5faf286eb0feb7395b8855",
+    "url": "/static/media/31.1e6a7afe.jpg"
+  },
+  {
+    "revision": "234f1f7b7247d10285b40dd1ce3702cf",
+    "url": "/static/media/32.234f1f7b.jpg"
+  },
+  {
+    "revision": "9fb69eac076ad92af669f6f75dd9b999",
+    "url": "/static/media/33.9fb69eac.jpg"
+  },
+  {
+    "revision": "23278498c90dbd48b2ad2b4ee8be0577",
+    "url": "/static/media/34.23278498.jpg"
+  },
+  {
+    "revision": "c5436bf79fdbda77f492b44acb0beeab",
+    "url": "/static/media/35.c5436bf7.jpg"
+  },
+  {
+    "revision": "a52f7c5791e7b830981956605131741e",
+    "url": "/static/media/36.a52f7c57.jpg"
+  },
+  {
+    "revision": "2ceba4eec1bd065a356fc3eb19ddc202",
+    "url": "/static/media/37.2ceba4ee.jpg"
+  },
+  {
+    "revision": "fb0e988bc773eb878e822655d08c845f",
+    "url": "/static/media/38.fb0e988b.jpg"
+  },
+  {
+    "revision": "a1c36bb8030069bf631c4841f09f293d",
+    "url": "/static/media/39.a1c36bb8.jpg"
+  },
+  {
+    "revision": "d5edb717500348f8405e5f1276a6593f",
+    "url": "/static/media/4.d5edb717.jpg"
+  },
+  {
+    "revision": "a010a3a02d5f9790bef39171c2c35816",
+    "url": "/static/media/40.a010a3a0.jpg"
+  },
+  {
+    "revision": "936c05dcb6cb9b7c120100f7741af184",
+    "url": "/static/media/5.936c05dc.jpg"
+  },
+  {
+    "revision": "2270d49b6be86bc1a2a3d265d85c5512",
+    "url": "/static/media/6.2270d49b.jpg"
+  },
+  {
+    "revision": "afa0fc4db0699d1e80744bc0ab6ef0df",
+    "url": "/static/media/7.afa0fc4d.jpg"
+  },
+  {
+    "revision": "809f66e52e3d8410e475dc999cb9347f",
+    "url": "/static/media/8.809f66e5.jpg"
+  },
+  {
+    "revision": "aa84b9c76484b0f14c61b346601f956f",
+    "url": "/static/media/9.aa84b9c7.jpg"
+  },
+  {
+    "revision": "89f248b2f7fbcdcf7ba6b41352c4f235",
+    "url": "/index.html"
+  }
+];
